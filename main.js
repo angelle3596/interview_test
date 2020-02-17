3ce679eea87cfe577b839266e548aea83db8a582
Tori Walker angelle3596@gmail.com <angelle3596@gmail.com>
2020-02-17 11:18:33 -0600

    implementation of the paddle

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const ctx = canvas.getContext("2d");
+let x=100;
+let y=200;
+var ballRadius = 10;
+
+let speedx=5;
+let speedy=5;
+let color = getRandomColor();
+const paddleHeight = 10;
+const paddleWidth= 75;
+let paddleX= (canvas.width-paddleWidth)/2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+const velocityDisplay = document.getElementById('velocity')
+
 // Initializes the canvas
 function initCanvas () {
     const canvas = document.getElementById('canvas');
@@ -9,12 +26,6 @@ function initCanvas () {
 }
 
 
-var x=100;
-var y=200;
-var speedx=5;
-var speedy=5;
-var color = getRandomColor();
-
 // Sets the context 
 function init()
 {
@@ -23,44 +34,77 @@ function init()
     const ctx = canvas.getContext('2d'); 
     ctx.clearRect(0,0, canvas.width, canvas.height)
     draw()
-  },10);
+},10);
+// const interval = setInterval(draw, 10);
+
 }
 // generates a random color for the ball
 function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    const letters = '0123456789ABCDEF'.split('');
+    let color = '#';
+    for (let i = 0; i < 6; i++ ) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
-// Draws the circle and sets the different directions everytime it bounces
+// Draws the circle 
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, 2 * Math.PI,true);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
+//Draws the paddle
+function drawPaddle () {
+    ctx.beginPath();
+    ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
 
+//Sets the different directions when the ball hits the wall
 function draw(){
-    var canvas = document.getElementById('canvas');
-    var circle = canvas.getContext("2d");
-    circle.beginPath();
-    circle.arc(x, y, 10, 0, 2 * Math.PI,true);
-    circle.closePath();
-    circle.fillStyle = color;
-    circle.fill();
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    drawBall();
     if( x<0 || x>canvas.width) {
         speedx=-speedx; 
         color = getRandomColor();
     }
-    if( y<0 || y>canvas.height) {
-        speedy=-speedy; 
-        color = getRandomColor();
-    } 
+
+    if(y + speedy < ballRadius) {
+        speedy = -speedy;
+    } else if(y + speedy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            speedy = -speedy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
+        }
+    }
     x+=speedx;
     y+=speedy;
+    //Moves the paddle and stops it when it hits the edge of the canvas
+    if(rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+    else if(leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0){
+            paddleX = 0;
+        }
+    }
+    drawPaddle();
   }
-document.addEventListener("DOMContentLoaded", () => {
-  initCanvas()
-  init()
-  draw()
-}); 
-
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+let interval= setInterval(draw,10);
 // Allows the up and down arrows to change the speed of the ball
 
 document.addEventListener("keydown",(event)=>{
@@ -72,12 +116,12 @@ document.addEventListener("keydown",(event)=>{
     if (event.keyCode === 38 ){
         (speedx > 0) ? speedx += 1 : speedx -= 1;
         (speedy > 0) ? speedy += 1 : speedy -= 1;
-        document.getElementById('velocity').innerText = Math.abs(speedx)
+        velocityDisplay.innerText = Math.abs(speedx)
     }
     if (event.keyCode === 40 ){
         (speedx > 0) ? speedx -= 1 : speedx += 1;
         (speedy > 0) ? speedy -= 1 : speedy += 1;
-        document.getElementById('velocity').innerText = Math.abs(speedx )
+        velocityDisplay.innerText = Math.abs(speedx )
     }
 })
 
@@ -91,15 +135,43 @@ document.getElementById("control-down").addEventListener("click",(event) =>{
     if (speedy > 0 || speedx > 0){
         (speedx > 0) ? speedx -= 1 : speedx += 1;
         (speedy > 0) ? speedy -= 1 : speedy += 1;
-        document.getElementById('velocity').innerText = Math.abs(speedx)
+        velocityDisplay.innerText = Math.abs(speedx)
     }
 })
 
 document.getElementById("control-up").addEventListener("click",(event) =>{
     (speedx > 0) ? speedx += 1 : speedx -= 1;
     (speedy > 0) ? speedy += 1 : speedy -= 1;
-    document.getElementById('velocity').innerText = Math.abs(speedx)
+    velocityDisplay.innerText = Math.abs(speedx)
 })
+
+
+//Functions for when you press a key it will let the paddle move left or right
+//When you release a key it won't allow the paddle to move left or right
+function keyDownHandler(event) {
+    if(event.key == "Right" || event.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(event.key == "Left" || event.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(event) {
+    if(event.key == "Right" || event.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(event.key == "Left" || event.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initCanvas()
+    init()
+    draw()
+    drawBall()
+  }); 
 
 
 
